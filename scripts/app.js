9af1dbdb9ff6c693f2144ef32018c482eb24e787
eleftheros <eleftheros_all@protonmail.com>
2021-04-22 09:28:37 +0300

    Add files

---
+++ scripts/app.js
@@ -0,0 +1,73 @@
+let scores, activePlayer, currentScore, isPlaying = false;
+const rollBtn = document.getElementById("btn-roll");
+const holdBtn = document.getElementById("btn-hold");
+const newBtn = document.getElementById("btn-new");
+const diceImg = document.querySelector(".dice");
+
+const init = () => {
+  currentScore = 0;
+  scores = [0, 0];
+  isPlaying = true;
+  activePlayer = Math.floor(Math.random() * 2);
+  document.querySelector(".player-score-0").textContent = scores[0];
+  document.querySelector(".player-score-1").textContent = scores[1];
+  document.querySelector(".current-score-0").textContent = currentScore;
+  document.querySelector(".current-score-1").textContent = currentScore;
+  document.getElementById("player-1").classList.remove("active");
+  document.getElementById("player-2").classList.remove("active");
+  document.getElementById("player-" + (activePlayer + 1)).classList.add("active");
+};
+
+const changeTurn = () => {
+  currentScore = 0;
+  document.querySelector(".current-score-" + activePlayer).textContent = currentScore;
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  document.getElementById("player-1").classList.toggle("active");
+  document.getElementById("player-2").classList.toggle("active");
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  init();
+});
+
+rollBtn.addEventListener("click", () => {
+  if (isPlaying) {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    diceImg.src = "assets/dice-" + dice + ".png";
+    
+    if(dice !== 1) {
+      currentScore += dice;
+    } 
+    else {
+      changeTurn();
+    }
+
+    document.querySelector(".current-score-" + activePlayer).textContent = currentScore;
+    }
+});
+
+holdBtn.addEventListener("click", () => {
+  if(isPlaying) {
+    let scoreElement = document.querySelector(".player-score-" + activePlayer);
+    scores[activePlayer] += currentScore;
+
+    if(scores[activePlayer] >= 100) {
+      scoreElement.textContent = "Winner!";
+      isPlaying = false;
+    }
+    else {
+      scoreElement.textContent = scores[activePlayer];
+      changeTurn();
+    }
+  }
+});
+
+newBtn.addEventListener("click", () => {
+  if(isPlaying) {
+    let newGame = confirm("Start New Game");
+    if(newGame) init();
+  } 
+  else {
+    init();
+  }
+})
